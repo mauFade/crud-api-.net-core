a8c26d70c6723795ae438ce01f8fcd7eedc344f3
Mauricio Cardoso <mauricio.cds00@gmail.com>
2022-08-27 19:58:10 -0300

    sql server config

---
+++ crud-api/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace crud_api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public SuperHero SuperHeroes { get; set; }
+    }
+}
